b52d25ba0b1795a6ba2072c098c782ea59906a50
SloppyLopez <sloppy@sloppylopez.com>
2015-11-30 00:10:31 +0100

    :tada: first commit!!!!

---
+++ templates/app.jsx
@@ -0,0 +1,9 @@
+import 'babel/polyfill'
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+    <p>JSPM React Rocks!!!</p>,
+    document.getElementById('root')
+);
---
+++ templates/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+var server = express();
+
+server.use(express.static('.'));
+server.use(express.static('views'));
+server.use(express.static('./jspm_packages'));
+
+server.get('/', function (req, res) {
+    res.render('index')
+});
+
+console.log('server listening on port: 3000');
+server.listen(3000);
