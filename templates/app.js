56847344b53f8719405757cd0c3799d9568396b9
sloppylopez <sloppy@sloppylopez.com>
2016-06-25 15:58:50 +0200

    fixed router

--- templates/app.js
+++ templates/app.js
@@ -10,228 +10,155 @@ import ToastrCss from 'stylesheets/toastr.css!css'
 import Toastr from 'toastr'
 
 import { render } from 'react-dom'
-import { Router, Route, IndexRoute, RouteHandler } from 'react-router'
-import { createHistory, useBasename } from 'history'
+import { Router, Route, IndexRoute, RouteHandler, browserHistory } from 'react-router'
 
 Toastr.warning('this website uses Cookies, please accept to continue reading', 'Elephant Seed', {timeOut: 5000000, closeButton: true});
 
-export default
-class App extends React.Component {
-    render() {
-        return (
-            <div>
-                <AcmeNavbar></AcmeNavbar>
 
-                <AcmeBody></AcmeBody>
+const App = ({ children }) => (
+    <div>
+        <AcmeNavbar></AcmeNavbar>
 
-                {this.props.children}
+        <AcmeBody></AcmeBody>
 
-                <AcmeFooter></AcmeFooter>
-            </div>
-        )
-    }
+        {children}
 
-    //arrow function automatically binds lexical "this"
-    // handleClick = () => {
-    //     //do something in response to button click
-    //     console.log('handled!');
-    // }
-}
+        <AcmeFooter></AcmeFooter>
+    </div>
+);
 
-class Index extends React.Component {
-    render() {
-        return (
-            <div className="container">
-                <ul className="features">
-                    <li>
-                        This is a React based template for building Next-Gen SPA Web applications in a blink
-                    </li>
-                    <li>
-                        Gulpless/Gruntless fully functional workflow just using JSPM as a universal tool
-                    </li>
-                    <li>
-                        Firebase hosting friendly, deploy your project using command line in seconds
-                    </li>
-                    <li>
-                        No build process, just run your desired web server and have fun playing
-                    </li>
-                    <li>
-                        Minimum configuration, whola lotta features for smooth develop
-                    </li>
-                    <li>
-                        Using ESlint the pluggable linting utility for JavaScript and JSX
-                    </li>
-                    <li>
-                        Isomorphic Client/Server React template rendering
-                    </li>
-                    <li>
-                        Convenient 7 to 1 Sass folder structure
-                    </li>
-                    <li>
-                        Babel ES6/ES* transpilation
-                    </li>
-                    <li>
-                        Efortless use
-                    </li>
-                    <li>
-                        <a href="https://www.npmjs.com/package/slush-jspm-react-seed">Fork U!</a>
-                    </li>
-                </ul>
-            </div>
-        )
-    }
-}
+const Index = () => (
+    <div className="container">
+        <ul className="features">
+            <li>
+                This is a React based template for building Next-Gen SPA Web applications in a blink
+            </li>
+            <li>
+                Gulpless/Gruntless fully functional workflow just using JSPM as a universal tool
+            </li>
+            <li>
+                Firebase hosting friendly, deploy your project using command line in seconds
+            </li>
+            <li>
+                No build process, just run your desired web server and have fun playing
+            </li>
+            <li>
+                Minimum configuration, whola lotta features for smooth develop
+            </li>
+            <li>
+                Using ESlint the pluggable linting utility for JavaScript and JSX
+            </li>
+            <li>
+                Isomorphic Client/Server React template rendering
+            </li>
+            <li>
+                Convenient 7 to 1 Sass folder structure
+            </li>
+            <li>
+                Babel ES6/ES* transpilation
+            </li>
+            <li>
+                Efortless use
+            </li>
+            <li>
+                <a href="https://www.npmjs.com/package/slush-jspm-react-seed">Fork U!</a>
+            </li>
+        </ul>
+    </div>
+);
 
-class Technologies extends React.Component {
-    render() {
-        return (
-            <div>
-                <h2>Technologies</h2>
-            </div>
-        )
-    }
-}
+const AcmeReact = () => (
+    <div>
+        <h2>React</h2>
 
-// class TechnologiesIndex extends React.Component {
-//     render() {
-//         return (
-//             <div>
-//                 <h3>TechnologiesIndex</h3>
-//             </div>
-//         )
-//     }
-// }
+        <ul className="features">
+            <li>
+                Modern library for developing user interfaces based entirely in Javascript
+            </li>
+        </ul>
+    </div>
+);
 
-class Technology extends React.Component {
-    render() {
-        return (
-            <div>
-                <h3>Technology {this.props.params.id}</h3>
-            </div>
-        )
-    }
-}
+const AcmeJspm = () => (
+    <div>
+        <h2>Jspm</h2>
 
-class AcmeReact extends React.Component {
-    render() {
-        return (
-            <div>
-                <h2>React</h2>
+        <ul className="features">
+            <li>
+                Load ES6, AMD, CommonJS and globals universally using your preferred online source
+            </li>
+            <li>
+                JSPM uses SystemJS module loader to provide ES6 loader features
+            </li>
+            <li>
+                Compile static resources in the browser in development time
+            </li>
+            <li>
+                Optimize static resources bundling them from production
+            </li>
+        </ul>
+    </div>
+);
 
-                <ul className="features">
-                    <li>
-                        Modern library for developing user interfaces based entirely in Javascript
-                    </li>
-                </ul>
-            </div>
-        )
-    }
-}
+const AcmeBabel = () => (
+    <div>
+        <h2>Babel</h2>
 
-class AcmeJspm extends React.Component {
-    render() {
-        return (
-            <div>
-                <h2>Jspm</h2>
+        <ul className="features">
+            <li>
+                Use the Javascript of tomorrow today, Babel transpiles your sources
+            </li>
+            <li>
+                No need to patiently wait for browser support
+            </li>
+        </ul>
+    </div>
+);
 
-                <ul className="features">
-                    <li>
-                        Load ES6, AMD, CommonJS and globals universally using your preferred online source
-                    </li>
-                    <li>
-                        JSPM uses SystemJS module loader to provide ES6 loader features
-                    </li>
-                    <li>
-                        Compile static resources in the browser in development time
-                    </li>
-                    <li>
-                        Optimize static resources bundling them from production
-                    </li>
-                </ul>
-            </div>
-        )
-    }
-}
+const AcmeNodejs = () => (
+    <div>
+        <h2>NodeJS</h2>
 
-class AcmeBabel extends React.Component {
-    render() {
-        return (
-            <div>
-                <h2>Babel</h2>
+        <ul className="features">
+            <li>
+                Runtime event-driven non-blocking I/O built on Chrome's V8 engine
+            </li>
+            <li>
+                Biggest eco-system of open source code in the whole planet
+            </li>
+        </ul>
+    </div>
+);
 
-                <ul className="features">
-                    <li>
-                        Use the Javascript of tomorrow today, Babel transpiles your sources
-                    </li>
-                    <li>
-                        No need to patiently wait for browser support
-                    </li>
-                </ul>
-            </div>
-        )
-    }
-}
+const AcmeReactBootstrao = () => (
+    <div>
+        <h2>React-Bootstrap</h2>
 
-class AcmeNodejs extends React.Component {
-    render() {
-        return (
-            <div>
-                <h2>NodeJS</h2>
+        <ul className="features">
+            <li>
+                Rebuilding of the preferred framework for front-end in React
+            </li>
+        </ul>
+    </div>
+);
 
-                <ul className="features">
-                    <li>
-                        Runtime event-driven non-blocking I/O built on Chrome's V8 engine
-                    </li>
-                    <li>
-                        Biggest eco-system of open source code in the whole planet
-                    </li>
-                </ul>
-            </div>
-        )
-    }
-}
+const AcmeESlint = () => (
+    <div>
+        <h2>ESlint</h2>
 
-class AcmeReactBootstrao extends React.Component {
-    render() {
-        return (
-            <div>
-                <h2>React-Bootstrap</h2>
-
-                <ul className="features">
-                    <li>
-                        Rebuilding of the preferred framework for front-end in React
-                    </li>
-                </ul>
-            </div>
-        )
-    }
-}
-
-class AcmeBootstrao extends React.Component {
-    render() {
-        return (
-            <div>
-                <h2>ESlint</h2>
-
-                <ul className="features">
-                    <li>
-                        Code linting is a type of static analysis that is frequently used to find problematic patterns
-                    </li>
-                    <li>
-                        It's a standard 'de facto' in all modern Web applications
-                    </li>
-                </ul>
-            </div>
-        )
-    }
-}
-
-const history = useBasename(createHistory)(
-    {basename: '/active-links'}
+        <ul className="features">
+            <li>
+                Code linting is a type of static analysis that is frequently used to find problematic patterns
+            </li>
+            <li>
+                It's a standard 'de facto' in all modern Web applications
+            </li>
+        </ul>
+    </div>
 );
 
 render((
-    <Router history={history}>
+    <Router history={browserHistory}>
         <Route path="/" component={App}>
             <IndexRoute component={Index}/>
             <Route path="/react" component={AcmeReact}/>
@@ -239,11 +166,7 @@ render((
             <Route path="/babel" component={AcmeBabel}/>
             <Route path="/nodejs" component={AcmeNodejs}/>
             <Route path="/react-bootstrap" component={AcmeReactBootstrao}/>
-            <Route path="/eslint" component={AcmeBootstrao}/>
-            <Route path="/technologies" component={Technologies}>
-                <IndexRoute component={Index}/>
-                <Route path=":id" component={Technology}/>
-            </Route>
+            <Route path="/eslint" component={AcmeESlint}/>
         </Route>
     </Router>
 ), document.getElementById('app'));
